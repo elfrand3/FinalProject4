990758840c4820f5e670e0978631284fdc7ba521
el_codee <elfrand3@gmail.com>
2022-12-08 21:18:36 +0700

    update

--- script.js
+++ script.js
@@ -8,13 +8,10 @@ searchButton.addEventListener('click', function() {
         .then(response => response.json())
         .then(response => {
             let result = document.querySelector('.result')
-
-            result.innerHTML = `<h2 style="margin-bottom: 15px;">${response.name}, ${response.sys.country}</h2>
-                                <h5><span class="temp">${response.main.temp}°С</span> <span class="temp">${response.weather[0].description}</span></h5>
-                                <p style="margin-bottom: 17px;">Temperature from ${response.main.temp_min}°С to ${response.main.temp_max}°С</p>
-                                <h5>Wind Speed : ${response.wind.speed} m/s</h5>
-                                <h5 style="margin-bottom: 17px;">Clouds : ${response.clouds.all}%</h5>
-                                <h4 style="color: #012443;">Geo Coordinates : [${response.coord.lat}, ${response.coord.lon}]</h4>` 
+            result.innerHTML = `<h2 style="margin-bottom: 15px;">City : ${response.name}</h2>
+                                <p style="margin-bottom: 17px;">Conditions : ${response.clouds.all}%</p>
+                                <p style="margin-bottom: 17px;">Temperature : ${response.main.temp_max}°С</p>
+                                <p>Wind Speed : ${response.wind.speed} m/s</p>`
         })
     inputKeyword.value = null;
 
